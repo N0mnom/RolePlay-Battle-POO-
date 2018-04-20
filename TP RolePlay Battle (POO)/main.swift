adb597f12e093b54f89c01defd3931850a2c72f2
N0mnom <nicolas.gallis@gmail.com>
2018-04-20 11:17:04 +0200

    Initial Commit

---
+++ TP RolePlay Battle (POO)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TP RolePlay Battle (POO)
+//
+//  Created by User on 20/04/18.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
